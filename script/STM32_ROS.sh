a31d82ddd0face5b22a4c292cdd238952c1df9a2
Titouan Melon <108010900+TitouanMelon@users.noreply.github.com>
2023-11-09 02:23:00 +0100

    Update STM32_ROS.sh

--- script/STM32_ROS.sh
+++ script/STM32_ROS.sh
@@ -1,4 +1,4 @@
 cd ./microros_ws
 source install/setup.bash
-source chmod 777 /dev/ttyUSB0
-ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0 -b 115200
+sudo chmod 777 /dev/ttyUSB0
+sudo ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0 -b 115200
