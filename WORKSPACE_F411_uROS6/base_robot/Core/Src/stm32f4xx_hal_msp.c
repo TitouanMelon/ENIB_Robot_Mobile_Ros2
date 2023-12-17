4373ff52a31b04b576d9d3deec3c16e359062c68
Titouan <titoumelon@gmail.com>
2023-12-17 19:57:47 +0100

    update ioc

--- WORKSPACE_F411_uROS6/base_robot/Core/Src/stm32f4xx_hal_msp.c
+++ WORKSPACE_F411_uROS6/base_robot/Core/Src/stm32f4xx_hal_msp.c
@@ -43,7 +43,7 @@ void HAL_MspInit(void)
 			ENCODER - TIMER1
 PWM1/1	-->	PA8		-- Encodeur Voie A
 PWM1/2	--> PA9		-- Encodeur Voie B
-EXTI1	--> PB10		-- Index encodeur
+EXTI10	--> PB10		-- Index encodeur
 ******************************************************************/
 void HAL_Encoder_Timer1_MspInit(void)
 {
@@ -57,13 +57,13 @@ void HAL_Encoder_Timer1_MspInit(void)
 	  GPIO_InitStruct.Speed = GPIO_SPEED_MEDIUM;
 	  GPIO_InitStruct.Alternate =  GPIO_AF1_TIM1 ; // hal_gpio_ex.h
 
-	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //PA8 & PA9
 
 	  GPIO_InitStruct.Pin = GPIO_PIN_10;
 	  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
 	  GPIO_InitStruct.Pull = GPIO_NOPULL;
 
-	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //PB10
 
 	  /* Enable and set EXTI Line0 Interrupt to the lowest priority */
 	  HAL_NVIC_SetPriority(EXTI15_10_IRQn, EXTI15_10_IRQ_PRIO, 0);
@@ -73,7 +73,7 @@ void HAL_Encoder_Timer1_MspInit(void)
 				ENCODER - TIMER2
 PWM2/1	-->	PA0		-- Encodeur Voie A
 PWM2/2	--> PA1		-- Encodeur Voie B
-EXTI 	--> PC0		-- Index Moteur
+EXTI0 	--> PC0		-- Index Moteur
 ******************************************************************/
 void HAL_Encoder_Timer2_MspInit(void)
 {
@@ -87,13 +87,13 @@ void HAL_Encoder_Timer2_MspInit(void)
 	  GPIO_InitStruct.Speed = GPIO_SPEED_MEDIUM;
 	  GPIO_InitStruct.Alternate =  GPIO_AF1_TIM2 ; // hal_gpio_ex.h
 
-	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //PA0 & PA1
 
 	  GPIO_InitStruct.Pin = GPIO_PIN_0;
 	  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
 	  GPIO_InitStruct.Pull = GPIO_NOPULL;
 
-	  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct); //PC0
 
 	  /* Enable and set EXTI Line0 Interrupt to the lowest priority */
 	  HAL_NVIC_SetPriority(EXTI0_IRQn, EXTI0_IRQ_PRIO, 0);
@@ -101,7 +101,7 @@ void HAL_Encoder_Timer2_MspInit(void)
 }
 
 /******************************************************************
-//			PWM - TIMER4 COMMANDE MOTEURS
+//			PWM - TIMER3 COMMANDE MOTEURS
 PA6 --> PWM3/1
 PC7 --> PWM3/2
 PB3 --> ENABLE MOTEUR (actif état Bas)
@@ -118,7 +118,7 @@ void HAL_PWM_Timer3_MspInit(void)
 	  GPIO_InitStruct.Speed = GPIO_SPEED_MEDIUM;
 	  GPIO_InitStruct.Alternate =  GPIO_AF2_TIM3 ; // hal_gpio_ex.h
 
-	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //PA6
 
 	  GPIO_InitStruct.Pin = GPIO_PIN_7;
 	  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -126,27 +126,24 @@ void HAL_PWM_Timer3_MspInit(void)
 	  GPIO_InitStruct.Speed = GPIO_SPEED_MEDIUM;
 	  GPIO_InitStruct.Alternate =  GPIO_AF2_TIM3 ; // hal_gpio_ex.h
 
-	  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct); //PC7
 
 	  // ENABLE MOTEUR : SORTIE LOGIQUE PB3
-	  GPIO_InitStruct.Pin = GPIO_PIN_7;
-	  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	  GPIO_InitStruct.Pull = GPIO_NOPULL;
-
-	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 1);
-
 	  GPIO_InitStruct.Pin = GPIO_PIN_3;
 	  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
 	  GPIO_InitStruct.Pull = GPIO_PULLUP;
 	  GPIO_InitStruct.Speed = GPIO_SPEED_FAST;
 
-	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //PB3
 	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, 1);
 
+	  /* ENABLE MOTEUR carte proto
+	  GPIO_InitStruct.Pin = GPIO_PIN_7;
+	  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	  GPIO_InitStruct.Pull = GPIO_NOPULL;
 
-
-
+	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 1);*/
 }
 
 /******************************************************************
@@ -165,14 +162,13 @@ void HAL_adcir_MspInit(void)
 	  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
 	  GPIO_InitStruct.Pull = GPIO_NOPULL;
 
-	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
+	  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //PA4
 
 	  GPIO_InitStruct.Pin = GPIO_PIN_0 ;
 	  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
 	  GPIO_InitStruct.Pull = GPIO_NOPULL;
 
-	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //PB0
 
 }
 
@@ -180,6 +176,9 @@ void HAL_adcir_MspInit(void)
 /******************************************************************
 * @brief I2C MSP Initialization
 * This function configures the hardware resources used in this example
+* I2C1 GPIO Configuration
+* PB8     ------> I2C1_SCL
+* PB9     ------> I2C1_SDA
 * @param hi2c: I2C handle pointer
 * @retval None
 ******************************************************************/
@@ -189,16 +188,13 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
   if(hi2c->Instance==I2C1)
   {
     __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**I2C1 GPIO Configuration
-    PB6     ------> I2C1_SCL
-    PB7     ------> I2C1_SDA
-    */
+
     GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //PB8 & PB9
 
     /* Peripheral clock enable */
     __HAL_RCC_I2C1_CLK_ENABLE();
@@ -207,11 +203,7 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
 	  HAL_NVIC_EnableIRQ(I2C1_ER_IRQn);
 	  HAL_NVIC_SetPriority(I2C1_EV_IRQn, I2C1_EV_IRQ_PRIO, 0);
 	  HAL_NVIC_EnableIRQ(I2C1_EV_IRQn);
-
-
-
   }
-
 }
 
 /******************************************************************
@@ -248,6 +240,13 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
 /******************************************************************
 * @brief UART MSP Initialization
 * This function configures the hardware resources used in this example
+* USART1 GPIO Configuration
+* PB6     ------> USART1_TX
+* PA10     ------> USART1_RX
+*
+* USART2 GPIO Configuration
+* PA2     ------> USART2_TX
+* PA3     ------> USART2_RX
 * @param huart: UART handle pointer
 * @retval None
 ******************************************************************/
@@ -263,23 +262,21 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     __HAL_RCC_USART1_CLK_ENABLE();
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**USART1 GPIO Configuration
-    PB6     ------> USART1_TX
-    PA10     ------> USART1_RX
-    */
+
+    // USART1
     GPIO_InitStruct.Pin = GPIO_PIN_10;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //PA10
 
     GPIO_InitStruct.Pin = GPIO_PIN_6;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct); //PB6
 
     /* USART1 DMA Init */
     /* USART1_RX Init */
@@ -334,16 +331,14 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     __HAL_RCC_USART2_CLK_ENABLE();
 
     __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**USART2 GPIO Configuration
-    PA2     ------> USART2_TX
-    PA3     ------> USART2_RX
-    */
+
+    // USART2
     GPIO_InitStruct.Pin = USART_TX_Pin|USART_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); //PA4 & PA8
 
     /* USART2 DMA Init */
     /* USART2_RX Init */
