cad0f38402e45a62441f22ac3a073d281a35d320
Titouan Melon <108010900+TitouanMelon@users.noreply.github.com>
2023-12-18 23:49:19 +0100

    Update STM32.sh

--- conception/installation scripts/STM32.sh
+++ conception/installation scripts/STM32.sh
@@ -1,4 +1,12 @@
-cd ./microros_ws
+DIR=$BASH_SOURCE
+
+if [ -L $DIR ]; then
+  DIR=$(dirname $(readlink $DIR))
+else
+  DIR=$(pwd)
+fi
+
+cd $DIR/microros_ws
 source install/setup.bash
 sudo chmod 777 /dev/ttyUSB0
 ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyUSB0 -b 115200
